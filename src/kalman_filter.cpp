e0341bf7e2a64e3a204063f42e5eb5fa19daa3de
spagad7 <shishir.pagad@gmail.com>
2018-06-11 00:43:06 -0700

    Adding files

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,61 @@
+#include "kalman_filter.h"
+#include <cmath>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+// Please note that the Eigen library does not initialize
+// VectorXd or MatrixXd objects with zeros upon creation.
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in)
+{
+    x = x_in;
+    P = P_in;
+    F = F_in;
+    H = H_in;
+    R = R_in;
+    Q = Q_in;
+    I = MatrixXd::Identity(H.cols(), H.cols());
+}
+
+void KalmanFilter::Predict()
+{
+    x = F * x;
+    P = F * P * F.transpose();
+}
+
+void KalmanFilter::Update(const VectorXd &z)
+{
+    MatrixXd H_tr = H.transpose();
+    VectorXd y = z - H*x;
+    MatrixXd S = H * P * H_tr + R;
+    MatrixXd K = P * H_tr * S.inverse();
+    x = x + K*y;
+    P = (I - K*H)*P;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z)
+{
+    // calculate h(x) vector to convert state vector values in cartesian coords
+    // to polar coords
+    float px = x(0);
+    float py = x(1);
+    float vx = x(2);
+    float vy = x(3);
+    float pxpy = pow(px, 2) + pow(py, 2);
+    float sq_pxpy = pow(pxpy, 0.5);
+    VectorXd h(3);
+    h << sq_pxpy, atan2(py, px), (px*vx + py*vy)/sq_pxpy;
+
+    MatrixXd H_j_tr = H_j.transpose();
+    VectorXd y = z - h*x;
+    MatrixXd S = H_j * P * H_j_tr + R;
+    MatrixXd K = P * H_j_tr * S.inverse();
+    x = x  + K*y;
+    P = (I - K*H)*P;
+}
---
+++ src/tools.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "tools.h"
+#include <cmath>
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+
+// Function to calculate Root Mean Square Error
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                                const vector<VectorXd> &ground_truth)
+{
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+    VectorXd temp(4);
+    int n = estimations.size();
+    if(n == 0)
+        return rmse;
+
+    for(int i=0; i<n; i++)
+    {
+        temp = estimations[i] - ground_truth[i];
+        temp = temp.array() * temp.array();
+        rmse += temp;
+    }
+    rmse = rmse/n;
+    rmse = rmse.array().sqrt();
+
+    return rmse;
+}
+
+//TODO: Consider converting MatrixXd to MatrixXf for more performance
+// Function to calculate Jacobian matrix from state vector
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state)
+{
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+    float pxpy = pow(px, 2) + pow(py, 2);
+
+    // check division by zero
+    if(pxpy == 0)
+    {
+        std::cerr << "Division by zero in Jacobian matrix!" << std::endl;
+        exit(-1);
+    }
+
+    float sq_pxpy = pow(pxpy, 0.5);
+    float cu_pxpy = pow(pxpy, 1.5);
+    float num_1 = py*(vx*py - vy*px);
+    float num_2 = px*(vy*px - vx*py);
+
+    // Fill Jacobian Matrix
+    MatrixXd J(3,4);
+    J   <<  px/sq_pxpy,     py/sq_pxpy,     0,          0,
+            -py/pxpy,       px/pxpy,        0,          0,
+            num_1/cu_pxpy,  num_2/cu_pxpy,  px/sq_pxpy, py/sq_pxpy;
+
+    return J;
+}
